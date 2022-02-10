b70ec79e3ec7dc1e8f937ef7926d437d37f6c846
LAPTOP-P3LH0THB\maxis <maxisuare3@gmail.com>
2022-02-10 10:18:27 -0300

    Arregle el css estaba probando

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
-    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
 
-
-
+<h1>Probando css</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et doloribus laboriosam autem provident porro possimus ab in voluptatem, enim consectetur velit perspiciatis aspernatur nesciunt incidunt fugiat, odio a iure? Temporibus.</p>
     <script src="js/master.js"></script>
 </body>
 
